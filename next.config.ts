0b40c8c21cfa058d5ef860af8e88db72ff8f7f45
Jirakrit <jayjirakrit@gmail.com>
2026-08-06 11:16:42 +0700

    update vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: 'https://serichai-qr-code.vercel.app', // Set the asset prefix to the Vercel deployment URL
 };
 
 export default nextConfig;
